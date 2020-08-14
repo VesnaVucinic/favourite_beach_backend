d708c984777454d62dd87080e2afea56ea4a4e49
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-14 14:29:20 +0100

    add index action in beaches controller

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::BeachesController < ApplicationController
+
+    def index
+        beaches = Beach.all #when I am dealing with views I have to pass instance variables to the view but renderin JSON don't eed instance variable can be just beach 
+        render json: beaches
+    end
+
+    
 end
