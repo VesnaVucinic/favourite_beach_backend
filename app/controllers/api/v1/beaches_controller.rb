9df876c43d7407247d3e5fe53984f5410a0e5b9f
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-14 16:05:04 +0100

    add beach and country serializers

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::BeachesController < ApplicationController
 
     def index
         beaches = Beach.all #when I am dealing with views I have to pass instance variables to the view but renderin JSON don't eed instance variable can be just beach 
-        render json: beaches
+        # render json: beaches
+        render json: BeachSerializer.new(beaches) #render new instance of beach serializer 
     end
 
     def create
