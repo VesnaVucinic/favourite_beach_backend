6514133cf859906d2dad8fc968bcbcdee7a9cd05
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-23 19:47:39 +0100

    add #auth_header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::API
         JWT.encode(payload, 'my_s3cr3t')
     end 
     
+    def auth_header
+        # { Authorization: 'Bearer <token>' }
+        request.headers['Authorization']
+    end
     
 
 end
