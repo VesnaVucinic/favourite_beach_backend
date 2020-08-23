c98b8aec9bd0780560418b1c6bcbf4a10f63e6d9
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-23 18:07:53 +0100

    add user_id to beaches and reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,5 @@ greece = Country.create(name: "Greece")
 spain = Country.create(name: "Spain")
 france = Country.create(name: "France")
 
-Beach.create(name: "Kassiopi", location: "Corfu", description: "This relatively remote but beloved beach is ideal for those who love diving from the rocks in a pool-like sea.", image_url: "https://iltesorivillas.com/wp-content/uploads/2017/12/rsz_il_tesori_villas_kerkyra-2.jpg", country_id: greece.id)
-Beach.create(name: "Vau Calanque D'En", location: "National Park of Calanques", description: "The beach is beautiful, stone, with cliffs on all sides, the sea with a stunning blue color, wonderful for the eyes and for the soul.", image_url: "https://www.turmundial.com/wp-content/uploads/2017/05/DSC_0282.jpg", country_id: france.id)
+Beach.create(name: "Kassiopi", location: "Corfu", description: "This relatively remote but beloved beach is ideal for those who love diving from the rocks in a pool-like sea.", image_url: "https://iltesorivillas.com/wp-content/uploads/2017/12/rsz_il_tesori_villas_kerkyra-2.jpg", country_id: greece.id, user_id:vesna.id )
+Beach.create(name: "Vau Calanque D'En", location: "National Park of Calanques", description: "The beach is beautiful, stone, with cliffs on all sides, the sea with a stunning blue color, wonderful for the eyes and for the soul.", image_url: "https://www.turmundial.com/wp-content/uploads/2017/05/DSC_0282.jpg", country_id: france.id, user_id: vesna.id)
