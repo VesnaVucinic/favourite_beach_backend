b1b679e5baf92ae39d005aa49d7d99be444b092f
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-23 17:12:01 +0100

    add validations, nas_secure_password and has_many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :beaches, dependent: :destroy
+
+
+    validates_presence_of :email
+    validates :email, uniqueness: { case_sensitive: false }
 end
