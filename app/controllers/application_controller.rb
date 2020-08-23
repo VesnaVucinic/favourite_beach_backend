0711369defe76c5351f285aefb131e17e529999d
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-23 19:45:25 +0100

    add #encode_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  
+    # STEP 1: Encode/Decode Tokens
+  # WHY?: `JWT.encode` takes up to three arguments: a payload to encode, an application secret of the developer's choice, and an optional third that can be used to specify the hashing algorithm used. Typically, we don't need to show the third. This method returns a JWT as a string.
+    def encode_token(payload)
+        # should store secret in env variable
+        JWT.encode(payload, 'my_s3cr3t')
+    end 
+    
+    
+
 end
