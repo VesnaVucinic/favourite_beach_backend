0c7363285685eb1b33bdad01b30d4a5bb16852b2
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-23 20:50:08 +0100

    add jwt in #create and skip_before_action :authorized

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-          # using built-in rails status codes
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
+      #byebug
+      @user = User.create(user_params)
+      if @user.valid?
+        # passing encode_token a payload of user id
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
     end
   
     private
@@ -15,5 +18,5 @@ class Api::V1::UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:email, :password)
     end 
-    
+
 end
