89f424b98215a8fce8e9253d1c75d005e70111a2
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-23 18:07:07 +0100

    add correction in validations

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -2,6 +2,6 @@ class Beach < ApplicationRecord
   belongs_to :country
   belongs_to :user
 
-  validates :name, :location, :description, :country_id, :image_url  presence: true
+  validates :name, :location, :description, :country_id, :image_url,  presence: true
 
 end
