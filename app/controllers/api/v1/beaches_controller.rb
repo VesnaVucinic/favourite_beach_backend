03bca7c62a6f80d16758b745ea929394694c8af5
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-08-31 14:44:43 +0100

    add show and update routes and actions

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -8,11 +8,25 @@ class Api::V1::BeachesController < ApplicationController
         render json: BeachSerializer.new(beaches) #render new instance of beach serializer 
     end
 
+    def show
+        beach = Beach.find_by(id: params[:id])
+        render json: BeachSerializer.new(beach), status: :accepted
+    end
+
     def create
         beach = current_user.beaches.build(beach_params)
         # beach = Beach.new(beach_params)
         if beach.save
             # byebug
+            render json: BeachSerializer.new(beach), status: :accepted
+        else
+            render json: {errors: beach.errors.full_messages}, status: :unprocessable_entity # server undertand request and syntax of requestis correct but validations faield
+        end
+    end
+
+    def update
+        beach = Beach.find_by(id: params[:id])
+        if beach.update(beach_params)
             render json: BeachSerializer.new(beach), status: :accepted
         else
             render json: {errors: beach.errors.full_messages}, status: :unprocessable_entity # server undertand request and syntax of requestis correct but validations faield
