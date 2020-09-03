cbc8425609868af794e62e0ed13f1db6fa1d42f4
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-09-03 18:11:22 +0100

    add id in beaach_params

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BeachesController < ApplicationController
 
-    skip_before_action :authorized, only: [:index]
+    skip_before_action :authorized, only: [:index, :show]
 
     def index
         beaches = Beach.all #when I am dealing with views I have to pass instance variables to the view but renderin JSON don't eed instance variable can be just beach 
@@ -26,6 +26,7 @@ class Api::V1::BeachesController < ApplicationController
 
     def update
         beach = Beach.find_by(id: params[:id])
+        # byebug
         if beach.update(beach_params)
             render json: BeachSerializer.new(beach), status: :accepted
         else
@@ -36,7 +37,7 @@ class Api::V1::BeachesController < ApplicationController
     private
 
     def beach_params
-        params.require(:beach).permit(:name, :location, :description, :image_url, :country_id)
+        params.require(:beach).permit(:id, :name, :location, :description, :image_url, :country_id)
     end
     
 end
